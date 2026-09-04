e45f95a7ba563e5ecd0d4de800b5d7394bba3a77
atharva kurle <kurleatharva2007@gmail.com>
2026-09-04 14:29:45 +0530

    ✅ Harshad Number | Easy | Python | Runtime: 0 ms (100.00%) | Memory: 19.3 MB | 2026-09-04

---
+++ leetcode/easy/3099-harshad-number/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
+        digits=list(map(int,str(x)))
+        h=sum(digits)
+        if x%h==0:
+          return h
+        else:
+          return -1
+          
